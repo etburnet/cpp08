54b22beae3e1cf92b93394be2299b01d9f22cc0f
etburnet <ethan.burnet.jaulmes@gmail.com>
2025-04-22 11:19:08 +0200

    ex01 done ready to push

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,23 +6,25 @@
 /*   By: eburnet <eburnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 13:35:15 by eburnet           #+#    #+#             */
-/*   Updated: 2025/04/21 15:19:42 by eburnet          ###   ########.fr       */
+/*   Updated: 2025/04/22 11:08:46 by eburnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stack>
 #include <iostream>
+#include <deque>
 
 template <typename T>
 class MutantStack : public std::stack<T>
 {
-	private:
-		
 	public:
+		typedef typename std::stack<T>::container_type::iterator iterator;
 		MutantStack();
 		MutantStack(const MutantStack &);
 		MutantStack &operator=(const MutantStack &);
 		~MutantStack();
+		iterator begin();
+		iterator end();
 };
 
 template <typename T>
@@ -30,19 +32,33 @@ MutantStack<T>::MutantStack()
 {
 }
 
-template<typename T>
+template <typename T>
 MutantStack<T>::~MutantStack()
 {
 }
 
-template<typename T>
-MutantStack<T>::MutantStack(const MutantStack &)
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack &other)
 {
-
+	this->operator=(other);
 }
 
-template<typename T>
-MutantStack<T> &MutantStack<T>::operator=(const MutantStack &)
+template <typename T>
+MutantStack<T> &MutantStack<T>::operator=(const MutantStack &other)
 {
+	if (this == &other)
+		return (*this);
+	return (*this);
+}
 
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin()
+{
+	return this->c.begin(); 
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end() 
+{
+	return this->c.end();
 }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eburnet <eburnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 13:35:13 by eburnet           #+#    #+#             */
-/*   Updated: 2025/04/21 15:21:27 by eburnet          ###   ########.fr       */
+/*   Updated: 2025/04/22 11:18:46 by eburnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,14 @@ int main()
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
+	std::cout << "Last elem added: " << mstack.top() << std::endl;
 	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+	std::cout << "Stack size: " << mstack.size() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
+	mstack.pop();
+	mstack.push(15);
 	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
@@ -32,9 +33,18 @@ int main()
 	--it;
 	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << "Iterator \"it\" value: " <<  *it << std::endl;
+		++it;
 	}
+	
+	std::cout << "My own tests" << std::endl;
+	it--;
+	it--;
+	std::cout << "Iterator \"it\" value: " <<  *it << std::endl;
+	--ite;
+	--ite;
+	std::cout << "Iterator \"ite\" value: " <<  *ite << std::endl;
+	ite = mstack.end();
 	std::stack<int> s(mstack);
 	return 0;
 }
